fa4e0337b33a21fadfc22a9affc62af2a696b502
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-05-27 21:56:25 -0300

    Implementando o metodo tocarMusica na classe IPhone.

--- DesafioPOO/src/IPhone/IPhone.java
+++ DesafioPOO/src/IPhone/IPhone.java
@@ -4,8 +4,12 @@ public class IPhone implements ReprodutoMusical {
 	
 	public static void main(String[] args) {
 		
-		
-		
 	}
+		
+		@Override
+		public void tocarMusica() {
+			System.out.println("Tocando a música: Insane ");
+			
+		}
 
 }
